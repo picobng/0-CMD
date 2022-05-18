58c09cd9884316d9857faff1058055c85817d59b
n0ne <98284987+N0n3-git@users.noreply.github.com>
2022-05-18 23:17:55 +0200

    Create led.sh

---
+++ led.sh
@@ -0,0 +1,27 @@
+#!  /bin/sh
+
+sudo sh -c "echo none > /sys/class/leds/led0/trigger"
+
+sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 1 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 1 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 1 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 1 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 1 > /sys/class/leds/led0/brightness"
+
+sleep 5
+
+sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 1 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 1 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 1 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 1 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 1 > /sys/class/leds/led0/brightness"
