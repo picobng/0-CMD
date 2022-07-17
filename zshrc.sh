8731e0faf45ee9906498f97d2f680e591caa925d
n0ne <98284987+N0n3-git@users.noreply.github.com>
2022-07-17 11:56:43 +0200

    Update zshrc.sh

--- zshrc.sh
+++ zshrc.sh
@@ -1,10 +1,18 @@
-## Alias
-alias ll='ls -l'
+# Folders
+alias ll='exa -la'
 alias la='ls -A'
 alias l='ls -CF'
 
+alias lll='ls -la'
+alias lla='ls -A'
+#alias l='ls -CF'
+
 alias maj='sudo apt-get update && echo -e \nUpgrade && sudo apt-get upgrade && echo -e \nFin.'
 alias majall='sudo apt-get update && echo -e \nUpgrade. && sudo apt-get upgrade && echo -e \nDist-Upgrade. && sudo apt-get dist-upgrade && echo -e \nFull-Upgrade'
+
+## Power
+alias reb='sudo reboot'
+alias shut='sudo shutdown now'
 
 alias gg="ping google.fr"
 alias log="tail -f /var/log/messages"
