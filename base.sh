1dabcbc2da2b8266435c3345162286b401c13dc9
n0ne <98284987+N0n3-git@users.noreply.github.com>
2022-07-17 18:51:33 +0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -27,20 +27,21 @@ sudo apt install neofetch -y
 
 ## Install Docker
 
+sudo apt remove docker docker-engine docker.io containerd runc -y
 sudo apt update
-sudo apt install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+sudo apt install ca-certificates curl gnupg lsb-release -y
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo groupadd docker
+sudo usermod -aG docker n0ne
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+sudo reboot
 
 ## Config locale
 sudo localectl set-locale fr_FR.UTF-8
