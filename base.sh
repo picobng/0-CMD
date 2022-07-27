174af3164fefeac92636858704e9476a79e48070
n0ne <98284987+N0n3-git@users.noreply.github.com>
2022-07-27 15:20:03 +0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -43,7 +43,8 @@ sudo apt install hdparm -y
 
 ## Install Docker
 
-sudo apt remove docker docker-engine docker.io containerd runc -y && sudo apt update && sudo apt install ca-certificates curl gnupg lsb-release -y && sudo mkdir -p /etc/apt/keyrings
+sudo apt remove docker docker-engine docker.io containerd runc -y && sudo apt update && sudo apt install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
