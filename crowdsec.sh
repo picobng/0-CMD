a0d41643887c87fda410566a56b8f9d849c000c5
n0ne <98284987+N0n3-git@users.noreply.github.com>
2022-08-03 14:40:01 +0200

    Create crowdsec.sh

---
+++ crowdsec.sh
@@ -0,0 +1,4 @@
+curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
+sudo apt-get install crowdsec
+sudo apt install crowdsec-firewall-bouncer-iptables
+sudo cscli console enroll cl6djq03x00000wl9mh1y9acd
