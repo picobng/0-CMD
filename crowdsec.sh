8c81cf128357f1a1fa8bfd90a4746c057d2d3438
n0ne <98284987+N0n3-git@users.noreply.github.com>
2022-08-03 14:41:56 +0200

    Update crowdsec.sh

--- crowdsec.sh
+++ crowdsec.sh
@@ -1,4 +1,4 @@
 curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
-sudo apt-get install crowdsec
-sudo apt install crowdsec-firewall-bouncer-iptables
+sudo apt-get install crowdsec -y
+sudo apt install crowdsec-firewall-bouncer-iptables -y
 sudo cscli console enroll cl6djq03x00000wl9mh1y9acd
