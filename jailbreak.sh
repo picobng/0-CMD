65535a9c4ea4393e5a58e78921eeacdc11e87b2a
n0ne <98284987+N0n3-git@users.noreply.github.com>
2022-11-26 01:59:01 +0100

    Update jailbreak.sh

--- jailbreak.sh
+++ jailbreak.sh
@@ -2,6 +2,7 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+brew install gcc -y
 brew install libimobiledevice libirecovery
 
 sudo apt install pip -y
@@ -10,3 +11,8 @@ mkdir iphone && cd iphone
 git clone --recursive https://github.com/palera1n/palera1n && cd palera1n
 
 sudo bash ./palera1n.sh --tweaks 15.7 --semi-tethered
+
+https://archlinux.org/packages/core/x86_64/openssl-1.1/
+cd ./usr/lib/
+sudo cp -r * /usr/lib/
+
