5e04250ed9b8d68ca2ba9bcab5a562871f9cdaf8
n0ne <98284987+N0n3-git@users.noreply.github.com>
2023-01-13 06:10:14 +0100

    Update base.sh

--- base.sh
+++ base.sh
@@ -43,16 +43,3 @@ sudo apt install exa tree neofetch lolcat screen htop -y
 ## Install Apps-2
 sudo apt install hdparm -y
 
-## Install Docker
-
-sudo apt remove docker docker-engine docker.io containerd runc -y && sudo apt update && sudo apt install ca-certificates curl gnupg lsb-release -y
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y && sudo groupadd docker
-sudo usermod -aG docker n0ne && mkdir -p /home/n0ne/.docker && sudo chown "n0ne":"n0ne" /home/n0ne/.docker -R && sudo chmod g+rwx "/home/n0ne/.docker" -R
-sudo systemctl enable docker.service && sudo systemctl enable containerd.service && sudo reboot
-cd .docker && docker run hello-world
-
