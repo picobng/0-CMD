b3708731e4fe4dbae67d4ecd3a28be26f06b2af3
n0ne <98284987+N0n3-git@users.noreply.github.com>
2023-01-31 00:28:13 +0100

    Update base.sh

--- base.sh
+++ base.sh
@@ -28,11 +28,19 @@ LC_ALL="fr_FR.UTF-8"" >> /etc/default/locale
 sudo localectl set-locale fr_FR.UTF-8 && sudo localectl set-keymap fr && sudo localectl set-x11-keymap fr
 sudo apt install manpages-fr manpages-fr-extra aptitude-doc-fr -y
 
-## Install ZSH, GIT, CURL
+### Install ZSH, GIT, CURL
 sudo apt install zsh git curl -y
 ## Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sed -i 's/robbyrussell/xiong-chiamiov-plus/g' .zshrc && zsh
+## Auto-completion
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+## HighLight
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+### Activation des plugins
+nano ~/.zshrc
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+
 
 #### Install CockPit
 sudo apt install -y cockpit cockpit-doc && cd /tmp && wget https://github.com/45Drives/cockpit-navigator/releases/download/v0.5.8/cockpit-navigator_0.5.8-1focal_all.deb && sudo apt install ./cockpit-navigator_0.5.8-1focal_all.deb && wget https://github.com/45Drives/cockpit-file-sharing/releases/download/v3.2.0/cockpit-file-sharing_3.2.0-1focal_all.deb && sudo apt install ./cockpit-file-sharing_3.2.0-1focal_all.deb -y && wget https://github.com/45Drives/cockpit-benchmark/releases/download/v0.2.1/cockpit-benchmark_0.2.1-1focal_all.deb && sudo apt install ./cockpit-benchmark_0.2.1-1focal_all.deb -y && cd /usr/share/cockpit && sudo git clone https://github.com/MRsagi/cockpit-temperature-plugin.git
